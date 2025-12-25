e01a255321266ef29000a21d23c6c89f402fbc6e
Deriech <dlcummings@aggies.ncat.edu>
2025-12-25 16:13:19 -0500

    rework design.v to properly ripple add

--- 1-ripple-carry-adder/design.v
+++ 1-ripple-carry-adder/design.v
@@ -1,19 +1,23 @@
+/*
+Project 1: 4-bit Ripple Carry Adder
+RTL design of a basic ripple carry adder using
+structutal modeling. Verify using testbench and
+waveform analysis
+*/
 module adder_4bit (input [3:0] a, b,
-                 input cin,
-                   output reg [3:0] out,
-                   output reg carry);
+                   input cin,
+                   output [3:0] out,
+                   output carry);
+  wire mid_carry[2:0];
+  adder add1(a[0], b[0], cin, out[0], mid_carry[0]);
+  adder add2(a[1], b[1], mid_carry[0], out[1], mid_carry[1]);
+  adder add3(a[2], b[2], mid_carry[1], out[2], mid_carry[2]);
+  adder add4(a[3], b[3], mid_carry[2], out[3], carry);
+endmodule
 
-  always @ (a or b or cin) begin
-    {carry, out} = a + b + cin;
-
-  end
-    
-  endmodule
-    
 module adder(input a,b,cin,
              output reg out, cout);
   always @ (a or b or cin) begin
-    out <= (a*b) + (b*cin) + (cin*a);
-    cout <= a ^ b ^ cin;
+    {cout, out} = a + b + cin;
   end
 endmodule
